10819c21586dff3581a3fe52640a6986a3ace190
pv297 <p.v.2977187@gmail.com>
2023-03-06 17:31:58 +0200

    test1

---
+++ src/test/java/Start_HW.java
@@ -0,0 +1,14 @@
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class Start_HW {
+    WebDriver wd;
+
+    @Test
+    public void start(){
+        wd = new ChromeDriver();
+        wd.get("https://www.google.com/");
+    }
+
+}
